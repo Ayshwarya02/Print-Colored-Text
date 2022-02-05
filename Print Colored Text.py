76601ed966d801d043521e4938d7fe00c8567dd7
Ayshwarya <86012289+Ayshwarya02@users.noreply.github.com>
2022-02-05 08:01:29 +0530

    Add files via upload

---
+++ Print Colored Text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Ayshwarya "+ Fore.YELLOW+ Back.BLUE+"I am studying in SRMIST KTR")
+print(Back.CYAN+"Hi My name is Ayshwarya")
+print(Fore.RED + Back.GREEN+ "Hi My name is Ayshwarya")
